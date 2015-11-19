912206df20019ea376afcdeac24c96cda60e6785
Joe Tsai <joetsai@digital-static.net>
2015-11-18 23:40:28 -0800

    internal/benchmark: call tick before running benchmark

--- internal/benchmark/main.go
+++ internal/benchmark/main.go
@@ -200,10 +200,10 @@ func runBenchmarks(files, codecs []string, tests, levels, sizes, fmts []int) {
 			// Progress ticker.
 			var cnt int
 			tick := func() {
-				cnt++
 				total := len(codecs) * len(files) * len(levels) * len(sizes)
 				pct := 100.0 * float64(cnt) / float64(total)
 				fmt.Printf("\t[%6.2f%%] %d of %d\r", pct, cnt, total)
+				cnt++
 			}
 
 			// Perform the benchmark. This may take some time.
