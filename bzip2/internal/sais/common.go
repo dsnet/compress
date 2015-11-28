71108ebe02a494396ddebf1cf17f01261c511771
Joe Tsai <joetsai@digital-static.net>
2015-11-28 01:11:02 -0800

    bzip2/internal/sais: improve documentation

--- bzip2/internal/sais/common.go
+++ bzip2/internal/sais/common.go
@@ -5,6 +5,20 @@
 // Package sais implements a linear time suffix array algorithm.
 package sais
 
+// This package ports the C sais implementation by Yuta Mori. The ports are
+// located in sais_byte.go and sais_int.go. Since Go does not support generics,
+// the implementations are copy and pastes of each other with minor changes
+// to the types used.
+//
+// The sais_int.go file can be generated from sais_byte.go with the the
+// following search-and-replace transformation:
+//	s/byte/int/g
+//
+// References:
+//	https://sites.google.com/site/yuta256/sais
+//	https://ge-nong.googlecode.com/files/Linear%20Time%20Suffix%20Array%20Construction%20Using%20D-Critical%20Substrings.pdf
+//	https://ge-nong.googlecode.com/files/Two%20Efficient%20Algorithms%20for%20Linear%20Time%20Suffix%20Array%20Construction.pdf
+
 // ComputeSA computes the suffix array of T and places the result in SA.
 // Both T and SA must be the same length.
 func ComputeSA(T []byte, SA []int) {
