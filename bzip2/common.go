f4bd2e6dca77737c14e5271cab3d4d9bcb00d0c3
Joe Tsai <joetsai@digital-static.net>
2015-11-28 01:32:29 -0800

    bzip2: clean-up common.go

--- bzip2/common.go
+++ bzip2/common.go
@@ -7,12 +7,29 @@ package bzip2
 
 import "runtime"
 import "hash/crc32"
+import "github.com/dsnet/compress/bzip2/internal"
 import "github.com/dsnet/golib/hashutil"
+
+// There does not exist a formal specification of the BZip2 format. As such,
+// much of this work is derived by either reverse engineering the original C
+// source code or using secondary sources.
+//
+// Compression stack:
+//	Run-length encoding 1     (RLE1)
+//	Burrows-Wheeler transform (BWT)
+//	Move-to-front transform   (MTF)
+//	Run-length encoding 2     (RLE2)
+//	Prefix encoding           (PE)
+//
+// References:
+//	http://bzip.org/
+//	https://en.wikipedia.org/wiki/Bzip2
+//	https://code.google.com/p/jbzip2/
 
 const (
 	hdrMagic = "BZ"
-	blkMagic = 0x314159265359
-	endMagic = 0x177245385090
+	blkMagic = 0x314159265359 // BCD of PI
+	endMagic = 0x177245385090 // BCD of sqrt(PI)
 
 	magicBits = 48
 )
@@ -40,50 +57,28 @@ func errRecover(err *error) {
 	}
 }
 
-var ReverseLUT [256]byte
-
-func init() {
-	for i := range ReverseLUT {
-		b := uint8(i)
-		b = (b&0xaa)>>1 | (b&0x55)<<1
-		b = (b&0xcc)>>2 | (b&0x33)<<2
-		b = (b&0xf0)>>4 | (b&0x0f)<<4
-		ReverseLUT[i] = b
-	}
-}
-
-// ReverseUint32 reverses all bits of v.
-func ReverseUint32(v uint32) (x uint32) {
-	x |= uint32(ReverseLUT[byte(v>>0)]) << 24
-	x |= uint32(ReverseLUT[byte(v>>8)]) << 16
-	x |= uint32(ReverseLUT[byte(v>>16)]) << 8
-	x |= uint32(ReverseLUT[byte(v>>24)]) << 0
-	return x
-
-}
-
 // updateCRC returns the result of adding the bytes in buf to the crc.
 func updateCRC(crc uint32, buf []byte) uint32 {
 	// The CRC-32 computation in bzip2 treats bytes as having bits in big-endian
 	// order. That is, the MSB is read before the LSB. Thus, we can use the
 	// standard library version of CRC-32 IEEE with some minor adjustments.
-	crc = ReverseUint32(crc)
+	crc = internal.ReverseUint32(crc)
 	var arr [4096]byte
 	for len(buf) > 0 {
 		cnt := copy(arr[:], buf)
 		buf = buf[cnt:]
 		for i, b := range arr[:cnt] {
-			arr[i] = ReverseLUT[b]
+			arr[i] = internal.ReverseLUT[b]
 		}
 		crc = crc32.Update(crc, crc32.IEEETable, arr[:cnt])
 	}
-	return ReverseUint32(crc)
+	return internal.ReverseUint32(crc)
 }
 
 // combineCRC combines two CRC-32 checksums together.
 func combineCRC(crc1, crc2 uint32, len2 int64) uint32 {
-	crc1 = ReverseUint32(crc1)
-	crc2 = ReverseUint32(crc2)
+	crc1 = internal.ReverseUint32(crc1)
+	crc2 = internal.ReverseUint32(crc2)
 	crc := hashutil.CombineCRC32(crc32.IEEE, crc1, crc2, len2)
-	return ReverseUint32(crc)
+	return internal.ReverseUint32(crc)
 }
