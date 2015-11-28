1af73f0839abffd2882c66466b3af803b6031f50
Joe Tsai <joetsai@digital-static.net>
2015-11-28 01:16:20 -0800

    bzip2: add skeleton for reader and writer

---
+++ bzip2/writer.go
@@ -0,0 +1,27 @@
+// Copyright 2015, Joe Tsai. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE.md file.
+
+package bzip2
+
+import "io"
+
+type writer struct{}
+
+func newWriter(w *io.Writer) *writer {
+	bw := new(writer)
+	bw.Reset(w)
+	return bw
+}
+
+func (bw *writer) Write(buf []byte) (int, error) {
+	return 0, nil
+}
+
+func (bw *writer) Close(buf []byte) error {
+	return nil
+}
+
+func (bw *writer) Reset(w *io.Writer) {
+	return
+}
