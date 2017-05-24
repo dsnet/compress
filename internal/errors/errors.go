c1be336ef9acf7a18cab1d08300e1f12e7400949
Joe Tsai <joetsai@digital-static.net>
2017-05-24 15:05:03 -0700

    internal/errors: add license header

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -1,3 +1,7 @@
+// Copyright 2016, Joe Tsai. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE.md file.
+
 // Package errors implements functions to manipulate compression errors.
 //
 // In idiomatic Go, it is an anti-pattern to use panics as a form of error
